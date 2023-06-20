a289b465f21f09ad3097fd70b0db1c8eb95f015d
Khin Kyaing Kyaing Thein <khinkyaingkyaingthein@gmial.com>
2023-06-20 17:56:59 -0400

    details section update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector(".navigation")
 const menuBtn = document.querySelector(".menu-btn")
 const closeBtn = document.querySelector(".close-btn")
 const openBtn = document.querySelector(".open-btn")
+const details = document.querySelector(".details")
 let isShow = true;
 
 
@@ -9,10 +10,12 @@ openBtn.addEventListener("click",()=>{
   navbar.classList.add("active")
   closeBtn.classList.add("show")
   openBtn.classList.add("open")
+  details.classList.add("modal-overlay")
 })
 closeBtn.addEventListener("click",()=>{
   navbar.classList.remove("active")
   closeBtn.classList.remove("show")
   openBtn.classList.remove("open")
+  details.classList.remove("modal-overlay")
 })
 
